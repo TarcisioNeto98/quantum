158586d6d0fe9e555509cff58bc37a86f3bc7aa7
TarcisioNeto98 <tarcisioneto1998@gmail.com>
2020-10-30 22:32:46 -0300

    Busca, alteração e remorção da entidade consulta adicionados ao backend

--- src/main/java/controller/ConsultaService.java
+++ src/main/java/controller/ConsultaService.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,6 +40,83 @@ public class ConsultaService extends HttpServlet {
 			return;
 		}
 		
+		if(path.contains("data")) {
+			String data = path.replace("/data/", "");
+			
+			System.out.println(data);
+			JSONArray jarray = new JSONArray();
+			List<Consulta> consultas = ConsultaDAO.getConsultaByData(data);
+			
+			if(consultas != null) {
+				for(Consulta consulta : consultas) {
+					JSONObject json = new JSONObject();
+					json.put("id", consulta.getId());
+					json.put("data", consulta.getData());
+					json.put("valor", consulta.getValor());
+					json.put("idDentista", consulta.getIdDentista());
+					json.put("idPaciente", consulta.getIdPaciente());
+					
+					jarray.put(json);
+				}
+				response.setStatus(200);
+			}
+			else response.setStatus(401);
+			
+			try {
+				System.out.println(jarray.toString());
+				PacienteService.enviarJSON(jarray.toString(), response);
+			}catch(Exception e) {
+				
+			}
+			return;
+		}
+		
+		if(path.contains("id")) {
+			int id = Integer.parseInt(path.replace("/id/", ""));
+			
+			System.out.println(id);
+			JSONObject json = new JSONObject();
+			Consulta consulta = ConsultaDAO.getConsulta(id);
+			
+			if(consulta != null) {
+				json.put("id", consulta.getId());
+				json.put("data", consulta.getData());
+				json.put("valor", consulta.getValor());
+				json.put("idDentista", consulta.getIdDentista());
+				json.put("idPaciente", consulta.getIdPaciente());
+				response.setStatus(200);
+			}
+			else response.setStatus(401);
+			
+			try {
+				System.out.println(json.toString());
+				PacienteService.enviarJSON(json.toString(), response);
+			}catch(Exception e) {
+				
+			}
+			return;
+		}
+		
+		JSONArray jarray = new JSONArray();
+		List<Consulta> consultas = ConsultaDAO.getAllConsultas();
+		
+		if(consultas != null) {
+			for(Consulta consulta : consultas) {
+				JSONObject json = new JSONObject();
+				json.put("id", consulta.getId());
+				json.put("data", consulta.getData());
+				json.put("idDentista", consulta.getIdDentista());
+				json.put("idPaciente", consulta.getIdPaciente());
+				
+				jarray.put(json);
+			}
+			try {
+				PacienteService.enviarJSON(jarray.toString(), response);
+			}catch(Exception e) {
+				
+			}
+			
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -57,7 +136,7 @@ public class ConsultaService extends HttpServlet {
 			// Request
 			jsonObject = new JSONObject(jb.toString());
 			if(Validar.salario(jsonObject.getString("valor"))) {
-				consulta = ConsultaDAO.addConsulta(jsonObject.getString("valor"), jsonObject.getString("data"),
+				consulta = ConsultaDAO.addConsulta(jsonObject.getString("data"),jsonObject.getString("valor"),
 				jsonObject.getInt("idPaciente"), Integer.parseInt(jsonObject.getString("idDentista")));
 				System.out.println(consulta.toString());
 				// Response
@@ -77,50 +156,49 @@ public class ConsultaService extends HttpServlet {
 		} catch (JSONException e) {
 		}
 		PacienteService.enviarJSON(jsonObject.toString(), response);
-
-		// UPDATE BY ID
-        /*String pathInfo = request.getPathInfo();
- 
-        if (pathInfo != null) {
-            String[] params = pathInfo.split("/");
- 
-            if (params.length > 0) {
-                StringBuffer jb = new StringBuffer();
-                String line = null;
-                try {
-                    BufferedReader reader = request.getReader();
-                    while ((line = reader.readLine()) != null)
-                        jb.append(line);
-                } catch (Exception e) {
-                }
- 
-                Consulta consulta = null;
-                JSONObject jsonObject = null;
- 
-                try {
-                    // Request
-                    jsonObject = new JSONObject(jb.toString());
-                    consulta = ConsultaDAO.updateConsulta(Integer.parseInt(params[1]), jsonObject.getString("data"),
-                    jsonObject.getInt("idPaciente"), jsonObject.getInt("idDentista"));
- 
-                    // Response
-                    jsonObject = new JSONObject();
-                    jsonObject.put("id", consulta.getId());
-    				jsonObject.put("data", consulta.getData());
-    				jsonObject.put("idPaciente", consulta.getIdPaciente());
-    				jsonObject.put("idDentista", consulta.getIdDentista());
- 
-                } catch (JSONException e) {
-                }
- 
-                response.setContentType("application/json");
-                response.setCharacterEncoding("UTF-8");
-                response.getWriter().print(jsonObject.toString());
-                response.getWriter().flush();
-            }
-        }*/
 	}
 	
+	
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = req.getReader();
+			while ((line = reader.readLine()) != null)
+				jb.append(line);
+		} catch (Exception e) {
+		}
+		
+		
+		try {
+			JSONObject json = new JSONObject(jb.toString());
+			
+			Consulta consulta = ConsultaDAO.updateConsulta(json.getInt("id"), json.getString("valor"),
+			json.getString("data"), json.getInt("idPaciente"), json.getInt("idDentista"));
+			
+			if(consulta != null) {
+				System.out.println(consulta);
+				json.put("id", consulta.getId());
+				json.put("valor", consulta.getValor());
+				json.put("data", consulta.getData());
+				json.put("idPaciente", consulta.getIdPaciente());
+				json.put("idDentista", consulta.getIdDentista());
+				resp.setStatus(200);
+			}
+			else resp.setStatus(401);
+			
+			PacienteService.enviarJSON(json.toString(), resp);
+			
+		}catch(Exception e) {
+			
+		}
+		
+	}
+
 	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
--- src/main/java/dao/ConsultaDAO.java
+++ src/main/java/dao/ConsultaDAO.java
@@ -1,33 +1,23 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import model.Consulta;
-import util.DbUtil;
 
 public class ConsultaDAO {
-	private static final Map<Integer, Consulta> userMap = new HashMap<Integer, Consulta>();
-	private static int i = 4;
-	private static Connection connection = DbUtil.getConnection();
 	
 	static EntityManagerFactory factory = Persistence.createEntityManagerFactory("clinicaodontologica");
     static EntityManager manager;
 	
-
 	public static Consulta getConsulta(int id) {
-		return userMap.get(id);
+		manager = factory.createEntityManager();
+		Consulta consulta = manager.find(Consulta.class, id);
+		manager.close();
+		return consulta;
 	}
 	
 	public static int quantidadeConsulta() {
@@ -39,7 +29,15 @@ public class ConsultaDAO {
         
         return Integer.parseInt(quantidade);
 	}
-
+	
+	public static List<Consulta> getConsultaByData(String data){
+		List<Consulta> consultas = null;
+		manager = factory.createEntityManager();
+		consultas = manager.createQuery("from Consulta WHERE data=?1", Consulta.class).setParameter(1, data).getResultList();
+		manager.close();
+		return consultas;
+	}
+	
 	public static Consulta addConsulta(String valor, String data, int idPaciente, int idDentista) {
 		
 		Consulta consulta = new Consulta(valor, data, idPaciente, idDentista);
@@ -51,22 +49,35 @@ public class ConsultaDAO {
         manager.close();
         
         return consulta;
+	}
+
+	public static Consulta updateConsulta(int id, String valor, String data, int idPaciente, int idDentista) {
+		Consulta consulta = new Consulta(id, valor, data, idPaciente, idDentista);
+		
+		manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.merge(consulta);
+		manager.getTransaction().commit();
+		manager.close();
+		return consulta;
 		
 	}
 
-	public static Consulta updateConsulta(int id, String data, int idPaciente, int idDentista) {
-		Consulta consulta = null;
-		userMap.put(consulta.getId(), consulta);
-		return consulta;
-	}
-
 	public static void deleteConsulta(int id) {
-		if (userMap.containsKey(id)) {
-			userMap.remove(id);
-		}
+		manager = factory.createEntityManager();
+		Consulta consulta = manager.find(Consulta.class, id);
+		
+		manager.getTransaction().begin();
+		manager.remove(consulta);
+		manager.getTransaction().commit();
+		manager.close();
 	}
 
 	public static List<Consulta> getAllConsultas() {
-		return new ArrayList<Consulta>(userMap.values());
+		List<Consulta> consultas = null;
+		manager = factory.createEntityManager();
+		consultas = manager.createQuery("FROM Consulta", Consulta.class).getResultList();
+		manager.close();
+		return consultas;
 	}
 }
